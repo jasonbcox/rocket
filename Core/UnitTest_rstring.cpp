b4936485748298c8a4104d3732806b1ad4eefd41
Jason B. Cox <contact@jasonbcox.com>
2013-09-07 21:17:15 +0000

    Fixed references to UnitTest_rstring

--- Core/UnitTest_rstring.cpp
+++ Core/UnitTest_rstring.cpp
@@ -1,13 +1,15 @@
 
-#include "UnitTest.h"
+#include "rocket/UnitTest.h"
 
-#include "string_.h"
+#include "rstring.h"
+
+using namespace Rocket::Core;
 
 Rocket_UnitTest ( String_Basic ) {
-	Rocket::Core::string str1 = "tester";
+	rstring str1 = "tester";
 	Rocket_UnitTest_Check_CharStringEqual( str1.c_str(), "tester" );
 
-	Rocket::Core::string str2 = "test";
+	rstring str2 = "test";
 	Rocket_UnitTest_Check_CharStringEqual( str2.c_str(), "test" );
 
 
@@ -21,17 +23,18 @@ Rocket_UnitTest ( String_Basic ) {
 	Rocket_UnitTest_Check_CharStringEqual( str1.c_str(), "tester" );
 
 	char str3_c[] = "cc* test";
-	Rocket::Core::string str3(str3_c);
+	rstring str3(str3_c);
 	Rocket_UnitTest_Check_CharStringEqual( str3.c_str(), "cc* test" );
 
 	str3_c[2] = '#';
 	Rocket_UnitTest_Check_CharStringEqual( str3.c_str(), "cc* test" );
 	Rocket_UnitTest_Check_CharStringEqual( str3_c, "cc# test" );
 
-	Rocket::Core::string str4;
+	rstring str4;
 	str4 << 1 << " " << 2 << " " << 3;
 	Rocket_UnitTest_Check_CharStringEqual( str4.c_str(), "1 2 3" );
 
-	Rocket::Core::string str5 = "test " + str4;
+	rstring str5 = "test " + str4;
 	Rocket_UnitTest_Check_CharStringEqual( str5.c_str(), "test 1 2 3" );
 }
+
